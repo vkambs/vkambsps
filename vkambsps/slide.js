1a39d01d984277cff1ca22a5c1073a40b546d797
vkambs <106272051+vkambs@users.noreply.github.com>
2023-04-24 00:02:14 -0400

    Slide changes

--- vkambsps/slide.js
+++ vkambsps/slide.js
@@ -1,24 +1,16 @@
-// Set the index of the current slide to 1
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Function to move to the previous or next slide
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Function to display the current slide
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
-  slides[slideIndex-1].style.display = "block";
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
